e8b888d376def6026de5184b236c88e69e77b9c2
dearguru07 <pangalaguruyadav101@gmail.com>
2024-10-05 08:04:40 +0530

    do

--- src/about.js
+++ src/about.js
@@ -4,8 +4,7 @@ const About=()=>{
     return <div className="pages">
         <h1>For better experience,download the Pizza Hub app now</h1>
             <div className="a-imgs">
-        <img className="l-i" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJ89jlLpiNDcvESPjnChG9VfymgrvGSyUwiQ&s"></img>
-
+            <img className="l-i" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJ89jlLpiNDcvESPjnChG9VfymgrvGSyUwiQ&s"></img>
             <img className="a-i" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/portal/m/play_store.png"></img>
             <img className="a-i" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/portal/m/app_store.png"></img>
             </div>
