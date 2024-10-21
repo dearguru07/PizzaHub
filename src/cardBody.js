89379f0e2b732e0b7142798e4f5c9916fc0849b8
dearguru07 <pangalaguruyadav101@gmail.com>
2024-10-21 13:22:41 +0530

    k

--- src/cardBody.js
+++ src/cardBody.js
@@ -1,5 +1,5 @@
 import React from "react";
-// import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom/client";
 import { useState } from "react";
 import { useEffect } from "react";
 import Shimmer from "./shimmer";
